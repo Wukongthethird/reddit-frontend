4c75fbd95886ee1194bcafcf456fa219e8104be1
Wukongthethird <simonzhang9518@gmail.com>
2024-06-10 16:54:51 -0700

    button has auth global state

--- src/components/Navbar/RightContent/AuthButtons.tsx
+++ src/components/Navbar/RightContent/AuthButtons.tsx
@@ -1,7 +1,12 @@
+import { authModalState } from "@/atoms/authMOdalAtom";
 import { Button } from "@chakra-ui/react";
 import React from "react";
+import { useSetRecoilState } from "recoil";
 
 const AuthButtons: React.FC = () => {
+  // only set State
+  const setAuthModalState = useSetRecoilState(authModalState);
+
   return (
     <>
       <Button
@@ -10,6 +15,7 @@ const AuthButtons: React.FC = () => {
         display={{ base: "none", sm: "flex" }}
         width={{ base: "70px", md: "110px" }}
         mr={2}
+        onClick={() => setAuthModalState({ open: true, view: "login" })}
       >
         Login{" "}
       </Button>
@@ -18,6 +24,7 @@ const AuthButtons: React.FC = () => {
         display={{ base: "none", sm: "flex" }}
         width={{ base: "70px", md: "110px" }}
         mr={2}
+        onClick={() => setAuthModalState({ open: true, view: "signup" })}
       >
         Signup
       </Button>
--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Flex } from "@chakra-ui/react";
 import AuthButtons from "./AuthButtons";
+import AuthModal from "@/components/Modal/Auth/AuthModal";
 
 type RightContextProps = {};
 
 const RightContent: React.FC<RightContextProps> = () => {
   return (
     <>
-      {/* <AuthModal/> */}
+      <AuthModal />
       <Flex justify={"center"} align={"center"}>
         <AuthButtons />
       </Flex>
