7e19a16f9f382ace04f66ad5fb73529ff32b5007
Wukongthethird <simonzhang9518@gmail.com>
2024-06-11 16:50:25 -0700

    styling for login and signup

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -9,9 +9,13 @@ import {
   ModalCloseButton,
   ModalBody,
   ModalFooter,
+  Flex,
+  Text,
 } from "@chakra-ui/react";
 import React from "react";
 import { useRecoilState } from "recoil";
+import AuthInputs from "./AuthInputs";
+import OAuthButtons from "./OAuthButtons";
 
 const AuthModal: React.FC = () => {
   // similar to state like useState
@@ -27,9 +31,33 @@ const AuthModal: React.FC = () => {
       <Modal isOpen={modalState.open} onClose={handleClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader textAlign={"center"}>
+            {modalState.view === "login" && "Login"}
+            {modalState.view === "signup" && "Sign Up"}
+            {modalState.view === "resetPassword" && "Reset Password"}
+          </ModalHeader>
           <ModalCloseButton />
-          <ModalBody>here is modal body</ModalBody>
+          <ModalBody
+            display="flex"
+            flexDirection="column"
+            alignItems={"center"}
+            justifyContent={"center"}
+            pb={6}
+          >
+            <Flex
+              direction={"column"}
+              align={"center"}
+              justify={"center"}
+              width="70%"
+            >
+              <OAuthButtons />
+              <Text color="gray.500" fontWeight={700}>
+                OR
+              </Text>
+              <AuthInputs />
+              {/* <ResetPassword/> */}
+            </Flex>
+          </ModalBody>
         </ModalContent>
       </Modal>
     </>
