40342a59946350dc8f6d2cbe3eb899f6c6dc44ab
Wukongthethird <simonzhang9518@gmail.com>
2024-06-20 23:15:20 -0700

    file upload form post form post atom

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -4,14 +4,16 @@ import { BiPoll } from "react-icons/bi";
 import { BsLink45Deg, BsMic } from "react-icons/bs";
 import { IoDocumentText, IoImageOutline } from "react-icons/io5";
 import TabItem from "./TabItem";
+import TextInputs from "./PostForm/TextInputs";
+import ImageUpload from "./PostForm/ImageUpload";
 
-const formTabs = [
+const formTabs: TabItem[] = [
   {
     title: "Post",
     icon: IoDocumentText,
   },
   {
-    title: "Image & Video",
+    title: "Images & Video",
     icon: IoImageOutline,
   },
   {
@@ -35,6 +37,49 @@ export type TabItem = {
 
 const NewPostForm: React.FC = () => {
   const [selectedTab, setSelectedTab] = useState(formTabs[0].title);
+  const [textInputs, setTextInputs] = useState({
+    title: "",
+    body: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const [selectedFile, setSelectedFile] = useState<string>();
+
+  const handleCreatePost = async () => {
+    //create new post object => type post
+    // store post in db
+    // check if selected file
+    // image goes to storage => getdownloadurl (return imageurl)
+    //update post doc by adding image url
+    // redirect back to communitypage using the router
+  };
+
+  const onSelectImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //js reads file method
+    const reader = new FileReader();
+
+    //event has file?
+    if (event.target.files?.[0]) {
+      // method to read data
+      reader.readAsDataURL(event.target.files[0]);
+    }
+    //once loaded cb function set on event to set state of file
+    reader.onload = (readerEvent) => {
+      if (readerEvent.target?.result) {
+        setSelectedFile(readerEvent.target.result as string);
+      }
+    };
+  };
+  const onTextChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const {
+      target: { name, value },
+    } = event;
+    setTextInputs((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
   return (
     <Flex direction={"column"} bg="white" borderRadius={4} mt={2}>
       <Flex width="100%">
@@ -46,6 +91,24 @@ const NewPostForm: React.FC = () => {
             setSelectedTab={setSelectedTab}
           />
         ))}
+      </Flex>
+      <Flex p={4}>
+        {selectedTab === "Post" && (
+          <TextInputs
+            loading={loading}
+            handleCreatePost={handleCreatePost}
+            textInputs={textInputs}
+            onChange={onTextChange}
+          />
+        )}
+        {selectedTab === "Images & Video" && (
+          <ImageUpload
+            selectedFile={selectedFile}
+            onSelectImage={onSelectImage}
+            setSelectedFile={setSelectedFile}
+            setSelectedTab={setSelectedTab}
+          />
+        )}
       </Flex>
     </Flex>
   );
